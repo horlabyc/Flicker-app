2ab56da6d0e3d3455bf921765a9e16eedfed74be
Sulaiman Olaosebikan <olabisisulaiman@gmail.com>
2021-07-22 14:52:45 +0100

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseurl: 'https://www.flickr.com/services/rest/?method=flickr.photos.search',
+  apiKey: '6ad82343b5f33d55812dc2208820cc2c'
 };
